3f0d400ba0297ee2df27a8138c3597bc99162ffe
Jessica Bohn <jessicabohn74@gmail.com>
2025-06-07 22:44:29 -0300

    Mão na massa - Criando uma calculadora: Iniciando o projeto

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace Calculator;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Clear(); // limpa a tela antes de iniciar a aplicação;
+        Console.WriteLine("Primeiro valor: "); // mensagem para o usuário;
+        float v1 = float.Parse(Console.ReadLine()); // float pois o usuário pode digitar um número quebrado, lembrando que o console retorna uma string então deve-se fazer a conversão com parse o Readline espera o usuário dar um enter;
+
+        Console.WriteLine(v1); //Exibe valor na tela
+    }
+}
